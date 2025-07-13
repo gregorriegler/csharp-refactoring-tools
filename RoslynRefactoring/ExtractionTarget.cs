e64058f134142092dfb75ed258c55362269136cd
Gregor Riegler <rieglerg85@gmail.com>
2025-07-13 23:07:59 +0200

    r add MethodSignature record type with Create factory method

--- RoslynRefactoring/ExtractionTarget.cs
+++ RoslynRefactoring/ExtractionTarget.cs
@@ -465,4 +465,21 @@ namespace RoslynRefactoring
 
         public bool RequiresReturnStatement => HasReturnStatements || AllPathsReturnOrThrow;
     }
+
+    public record MethodSignature
+    {
+        public BlockSyntax MethodBody { get; }
+        public TypeSyntax ReturnType { get; }
+
+        private MethodSignature(BlockSyntax methodBody, TypeSyntax returnType)
+        {
+            MethodBody = methodBody;
+            ReturnType = returnType;
+        }
+
+        public static MethodSignature Create(BlockSyntax methodBody, TypeSyntax returnType)
+        {
+            return new MethodSignature(methodBody, returnType);
+        }
+    }
 }
