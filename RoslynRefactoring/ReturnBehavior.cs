6d61214672f92ceabf68566ecf1261fc38404a94
Gregor Riegler <rieglerg85@gmail.com>
2025-07-17 19:48:38 +0200

    r

--- RoslynRefactoring/ReturnBehavior.cs
+++ RoslynRefactoring/ReturnBehavior.cs
@@ -3,22 +3,22 @@ using Microsoft.CodeAnalysis.CSharp.Syntax;
 namespace RoslynRefactoring;
 
 public class ReturnBehavior
+{
+    private readonly List<StatementSyntax> selectedStatements;
+
+    public ReturnBehavior(List<StatementSyntax> statements)
     {
-        private readonly List<StatementSyntax> selectedStatements;
+        selectedStatements = statements;
+    }
 
-        public ReturnBehavior(List<StatementSyntax> statements)
-        {
-            selectedStatements = statements;
-        }
+    public bool RequiresReturnStatement => HasReturnStatements || AllPathsReturnOrThrow;
 
-        public bool HasReturnStatements => selectedStatements
-            .SelectMany(stmt => stmt.DescendantNodesAndSelf().OfType<ReturnStatementSyntax>())
-            .Any();
+    private bool HasReturnStatements => selectedStatements
+        .SelectMany(stmt => stmt.DescendantNodesAndSelf().OfType<ReturnStatementSyntax>())
+        .Any();
 
-        public bool AllPathsReturnOrThrow => selectedStatements is [SwitchStatementSyntax switchStatement]
-                                             && switchStatement.Sections.All(sec =>
-                                                 sec.Statements.LastOrDefault() is ReturnStatementSyntax
-                                                     or ThrowStatementSyntax);
-
-        public bool RequiresReturnStatement => HasReturnStatements || AllPathsReturnOrThrow;
+    private bool AllPathsReturnOrThrow => selectedStatements is [SwitchStatementSyntax switchStatement]
+                                          && switchStatement.Sections.All(sec =>
+                                              sec.Statements.LastOrDefault() is ReturnStatementSyntax
+                                                  or ThrowStatementSyntax);
 }
