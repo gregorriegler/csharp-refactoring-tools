f009691d5fe4b012b1fca242f42c2ac21c27be5b
Gregor Riegler <rieglerg85@gmail.com>
2025-08-10 11:55:36 +0200

    t move test data inline in ExtractMethodInvalidSelectionTests for better readability

--- RoslynRefactoring.Tests/ExtractMethodInvalidSelectionTests.cs
+++ RoslynRefactoring.Tests/ExtractMethodInvalidSelectionTests.cs
@@ -10,39 +10,7 @@ public class ExtractMethodInvalidSelectionTests
     [Test]
     public async Task ShouldHandleInvalidSelectionCrossingStatementBoundaries()
     {
-        var code = CreateTestClassWithMethodCall();
-        var selection = CodeSelection.Parse("6:8-8:17");
-
-        await VerifyExtract(code, selection, "ExtractedMethod");
-    }
-
-    private static async Task VerifyExtract(string code, CodeSelection codeSelection, string newMethodName)
-    {
-        var result = await ExtractMethodTestHelper.PerformExtractAndFormat(code, codeSelection, newMethodName);
-        await Verify(result);
-    }
-
-    [Test]
-    public async Task ShouldHandleInvalidSelectionFromMiddleOfParameterToNextStatement()
-    {
-        var code = CreateTestClassWithMethodCall();
-        var selection = CodeSelection.Parse("7:12-8:17");
-
-        await VerifyExtract(code, selection, "ExtractedMethod");
-    }
-
-    [Test]
-    public async Task ShouldHandleMissingVariableDependencies()
-    {
-        var code = CreateTestClassWithVariableDependency();
-        var selection = CodeSelection.Parse("9:8-9:40");
-
-        await VerifyExtract(code, selection, "ExtractedMethod");
-    }
-
-    private static string CreateTestClassWithMethodCall()
-    {
-        return """
+        var code = """
             public class TestClass
             {
                 public void TestMethod()
@@ -59,11 +27,46 @@ public class ExtractMethodInvalidSelectionTests
                 }
             }
             """;
+        var selection = CodeSelection.Parse("6:8-8:17");
+
+        await VerifyExtract(code, selection, "ExtractedMethod");
     }
 
-    private static string CreateTestClassWithVariableDependency()
+    private static async Task VerifyExtract(string code, CodeSelection codeSelection, string newMethodName)
     {
-        return """
+        var result = await ExtractMethodTestHelper.PerformExtractAndFormat(code, codeSelection, newMethodName);
+        await Verify(result);
+    }
+
+    [Test]
+    public async Task ShouldHandleInvalidSelectionFromMiddleOfParameterToNextStatement()
+    {
+        var code = """
+            public class TestClass
+            {
+                public void TestMethod()
+                {
+                    var x = SomeMethod(
+                        parameter1,
+                        parameter2);
+                    var y = x + 1;
+                }
+
+                private int SomeMethod(string param1, string param2)
+                {
+                    return 42;
+                }
+            }
+            """;
+        var selection = CodeSelection.Parse("7:12-8:17");
+
+        await VerifyExtract(code, selection, "ExtractedMethod");
+    }
+
+    [Test]
+    public async Task ShouldHandleMissingVariableDependencies()
+    {
+        var code = """
             public class TestClass
             {
                 public void TestMethod()
@@ -82,6 +85,10 @@ public class ExtractMethodInvalidSelectionTests
                 }
             }
             """;
+        var selection = CodeSelection.Parse("9:8-9:40");
+
+        await VerifyExtract(code, selection, "ExtractedMethod");
     }
+
 
 }
